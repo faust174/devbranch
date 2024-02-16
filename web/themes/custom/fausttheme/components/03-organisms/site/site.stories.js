cf845187ba772185fe19781d6dd9739624ea2dbc
faust174 <m.soloninko@dev-branch.com>
2024-02-16 18:27:09 +0200

    created header components with storybook

--- web/themes/custom/fausttheme/components/03-organisms/site/site.stories.js
+++ web/themes/custom/fausttheme/components/03-organisms/site/site.stories.js
@@ -1,12 +1,19 @@
 import footerTwig from './site-footer/site-footer.twig';
-import siteHeader from './site-header/site-header.twig';
+import siteHeader from './site-header/header.twig';
+// import siteHeader from './site-header/site-header.twig';
 
 import footerSocial from '../../02-molecules/menus/social-menu/social-menu.yml';
 import footerMenu from '../../02-molecules/menus/inline/inline-menu.yml';
 import breadcrumbData from '../../02-molecules/menus/breadcrumbs/breadcrumbs.yml';
 import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
 
+import leftHeaderMenuData from '../../02-molecules/menus/left-header-menu/left-header-menu.yml';
+import rightHeaderMenuData from '../../02-molecules/menus/right-header-menu/right-header-menu.yml';
+import siteLogoData from '../../01-atoms/text/text/logo.yml';
+
 import '../../02-molecules/menus/main-menu/main-menu';
+// import '../../02-molecules/menus/right-header-menu/right-header-menu.yml';
+// import '../../02-molecules/menus/left-header-menu/left-header-menu.yml';
 
 /**
  * Storybook Definition.
@@ -24,4 +31,7 @@ export const header = () =>
   siteHeader({
     ...breadcrumbData,
     ...mainMenuData,
+    ...leftHeaderMenuData,
+    ...rightHeaderMenuData,
+    ...siteLogoData,
   });
