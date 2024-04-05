577a0380e9d57cc3c8ccc553764b193dee8f02e1
faust174 <m.soloninko@dev-branch.com>
2024-04-05 15:02:52 +0300

    7829.main. Changed variable declarations to const. Changed the once function, so the map renders correctly. Changed the way leaflet library installed

--- web/modules/custom/stores/js/map.js
+++ web/modules/custom/stores/js/map.js
@@ -1,24 +1,23 @@
 (function (Drupal, once) {
   Drupal.behaviors.storesMap = {
     attach: function (context, settings) {
-      once('storesMap', '#map', context).forEach(function () {
+      once('storesMap', '#map', context).forEach(function (element) {
+        const options = settings.stores;
+        const color = options.color;
+        const size = options.size;
+        const zoom = options.zoom;
+        const locations = options.locations;
 
-        let options = settings.stores;
-        let color = options.color;
-        let size = options.size;
-        let zoom = options.zoom;
-        let locations = options.locations;
-
-        document.getElementById('map').style.height = '400px';
-        let map = L.map('map').setView([51.505, -0.09], zoom);
+        element.style.height = '400px';
+        const map = L.map(element).setView([51.505, -0.09], zoom);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 20,
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
         locations.forEach(function(location) {
-          let lat = location[0].lat;
-          let lon = location[0].lon;
+          const lat = location[0].lat;
+          const lon = location[0].lon;
 
           L.circleMarker([lat, lon], {
             color: color,
