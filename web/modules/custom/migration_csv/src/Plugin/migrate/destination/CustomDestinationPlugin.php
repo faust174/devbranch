db8d84b150374428f54e70a06f15fa9cbbb82bac
faust174 <m.soloninko@dev-branch.com>
2024-04-09 11:45:32 +0300

    7794.main.Added DI. Changed the import logic, so it uses the processed data.

--- web/modules/custom/migration_csv/src/Plugin/migrate/destination/CustomDestinationPlugin.php
+++ web/modules/custom/migration_csv/src/Plugin/migrate/destination/CustomDestinationPlugin.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\migration_csv\Plugin\migrate\destination;
 
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\Plugin\migrate\destination\DestinationBase;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a destination plugin for migrating data to a user_data table.
@@ -12,7 +16,24 @@ use Drupal\migrate\Row;
  *   id = "custom_destination_plugin"
  * )
  */
-class CustomDestinationPlugin extends DestinationBase {
+class CustomDestinationPlugin extends DestinationBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, protected Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
+    return new static(
+    $configuration,
+    $plugin_id,
+    $plugin_definition,
+    $migration,
+    $container->get('database'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -24,17 +45,18 @@ class CustomDestinationPlugin extends DestinationBase {
       ],
     ];
   }
+
   /**
    * {@inheritDoc}
    */
   public function import(Row $row, array $old_destination_id_values = []):void {
     $data = [
-      'uid' => $row->getSourceProperty('uid'),
-      'city' => $row->getSourceProperty('city'),
-      'country' => $row->getSourceProperty('country'),
+      'uid' => $row->getDestinationProperty('uid'),
+      'city' => $row->getDestinationProperty('city'),
+      'country' => $row->getDestinationProperty('country'),
     ];
 
-    \Drupal::database()->insert('user_data')
+    $this->database->insert('user_data')
       ->fields($data)
       ->execute();
   }
